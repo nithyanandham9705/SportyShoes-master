5a1eb5ca6e33ffa49bd6c69d4fcf3e27069eedc0
nithyanandham9705 <90197054+nithyanandham9705@users.noreply.github.com>
2023-04-01 07:07:37 +0530

    Update ApplicationTests.java

--- SportyShoes.com/src/test/java/com/aman/phase3/ApplicationTests.java
+++ SportyShoes.com/src/test/java/com/aman/phase3/ApplicationTests.java
@@ -1,7 +1,7 @@
 package com.aman.phase3;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTes;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
